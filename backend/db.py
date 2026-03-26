e2371facf37435d6e618e6933d206bba3bf217f2
Sanikagav <20220802038@dypiu.ac.in>
2026-03-26 23:49:37 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,15 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine(
-    "postgresql://postgres:password@localhost/patientdb"
+DATABASE_URL = "postgresql://postgres:2584@localhost:5432/dmhdb"
+
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
+
+Base = declarative_base()
